2394663d0c72f46af084810a0c581593c8957cb4
SebastianHajdus <sebastian.hajdus@gmail.com>
2016-12-07 10:31:47 +0100

    dropdownmenu modification

---
+++ js/app_test.js
@@ -0,0 +1,42 @@
+$(function () {
+
+console.log('dziala');
+
+
+// --- dropdownmenu --- //
+
+
+        var button = document.querySelector('#menu_logo');
+        console.log(button);
+
+        var list = document.querySelector('.menu-content');
+        console.log(list);
+
+//wylaczanie po kliknieciu w linki
+
+        var a = document.querySelectorAll('.links');
+        console.log(a);
+
+        for( i=0; i<a.length; i++) {
+          a[i].addEventListener('click', function() {
+              list.style.display = 'none';
+          });
+        }
+
+//wlaczanie guzikiem, pozadane zeby na starcie wlaczal gdy jest wylaczony, a wylaczal gdy jet wlaczony
+
+        button.addEventListener('click', function() { //na guzik nadaje klikanie
+            if (list.style.display = 'block') {
+
+            } else {
+              list.style.display = 'none';
+            }
+
+
+
+        });
+
+
+
+
+});
